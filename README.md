e9445339de2846153d3537fa3422521d43dc2813
aiplaybookin <aiplaybook.in@gmail.com>
2023-06-02 18:40:38 +0000

    updated readme, used BLACK

--- README.md
+++ README.md
@@ -16,10 +16,77 @@ In this assignment, you will:
 4. Save the trained model checkpoint to the host operating system.
 5. Add an option to resume model training from a checkpoint.
 
-## Starter Code
+## How to run the code
 
-The provided starter code in train.py provides a basic structure for loading data, defining a model, and running training and testing loops. You will need to complete the code at locations marked by TODO: comments.
+1. Login to [Dockerhub](https://hub.docker.com "visit here") 
+2. Create an account
+3. Go to *Account Settings* and create a access token 
+4. From your local machine/ gitpod etc wherever the code is ( and has docker installed) 
+```
+docker login -u vikashkr117
+```
+and enter the *password*
 
-## Submission
+6. To build the docker image
+```
+docker build --tag vikashkr117/docker-demo-2023 .
+```
 
-When you have completed the assignment, push your code to your Github repository. The Github Actions workflow will automatically build your Docker image, run your training script, and check if the assignment requirements have been met. Check the Github Actions tab for the results of these checks. Make sure that all checks are passing before you submit the assignment.
+7. To run the docker
+```
+docker run --rm vikashkr117/docker-demo-2023
+```
+
+8. To run test cases
+```
+bash ./tests/grading.sh
+```
+
+9. To push to DockerHub
+```
+docker push vikashkr117/docker-demo-2023
+```
+
+10. To pull from Dockerhub
+```
+docker pull vikashkr117/docker-demo-2023
+```
+
+
+
+## Use *BLACK* to format code
+1. Install
+```
+pip install black
+```
+
+2. To format the scripts in current directory
+```
+black .
+```
+
+## Other reference commands
+To remove 
+  - all stopped containers
+  - all networks not used by at least one container
+  - all dangling images
+  - all dangling build cache
+```
+docker system prune  // to remove any 
+```
+
+```
+docker ps                  // ps to see running containers
+docker image ls            // to see all images built
+docker run -it ubuntu ls
+docker run -it ubuntu bash  // access inside the container : host fliesystem
+docker exec -it <name of running container> <command to execute>    //e.g.  docker exec -it stotic bash
+docker ps -a     // all container that was run and existed, it also shows command run in that
+docker ps -aq    // all container id only that was run and existed, it also shows command run in that
+// mount volume (code folder etc) to a docker
+docker run -it -v <local path>:<docker path> ubuntu bash
+// to remove all previous exited containers 
+docker rm $(docker ps -aq)
+docker inspect ubuntu // look inside the image details
+docker run -p 80:5000 catgif          // route port 80 of host machine to port 5000 of conatiner
+```
--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import argparse
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
+
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -21,6 +22,7 @@ class Net(torch.nn.Module):
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
+
 def train_epoch(epoch, args, model, device, data_loader, optimizer):
     model.train()
     for batch_idx, (data, target) in enumerate(data_loader):
@@ -31,8 +33,11 @@ def train_epoch(epoch, args, model, device, data_loader, optimizer):
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(data_loader.dataset)} '
-                  f'({100. * batch_idx / len(data_loader):.2f}%)]\tLoss: {loss.item():.6f}')
+            print(
+                f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(data_loader.dataset)} "
+                f"({100. * batch_idx / len(data_loader):.2f}%)]\tLoss: {loss.item():.6f}"
+            )
+
 
 def test_epoch(model, device, data_loader):
     model.eval()
@@ -42,36 +47,75 @@ def test_epoch(model, device, data_loader):
         for data, target in data_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            test_loss += F.nll_loss(output, target, reduction="sum").item()
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(data_loader.dataset)
-    accuracy = 100. * correct / len(data_loader.dataset)
-    print(f'Test set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(data_loader.dataset)} '
-          f'({accuracy:.2f}%)')
+    accuracy = 100.0 * correct / len(data_loader.dataset)
+    print(
+        f"Test set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(data_loader.dataset)} "
+        f"({accuracy:.2f}%)"
+    )
+
 
 def main():
     # Parser to get command line arguments
-    parser = argparse.ArgumentParser(description='MNIST Training Script')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
-    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                        help='SGD momentum (default: 0.5)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--resume', action='store_true', default=False,
-                        help='resume training from the last checkpoint')
+    parser = argparse.ArgumentParser(description="MNIST Training Script")
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=10,
+        metavar="N",
+        help="number of epochs to train (default: 10)",
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=0.01,
+        metavar="LR",
+        help="learning rate (default: 0.01)",
+    )
+    parser.add_argument(
+        "--momentum",
+        type=float,
+        default=0.5,
+        metavar="M",
+        help="SGD momentum (default: 0.5)",
+    )
+    parser.add_argument(
+        "--no-cuda", action="store_true", default=False, help="disables CUDA training"
+    )
+    parser.add_argument(
+        "--seed", type=int, default=1, metavar="S", help="random seed (default: 1)"
+    )
+    parser.add_argument(
+        "--log-interval",
+        type=int,
+        default=10,
+        metavar="N",
+        help="how many batches to wait before logging training status",
+    )
+    parser.add_argument(
+        "--resume",
+        action="store_true",
+        default=False,
+        help="resume training from the last checkpoint",
+    )
 
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
@@ -79,37 +123,44 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     # Load the MNIST dataset for training and testing
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    )
 
-    train_dataset = datasets.MNIST('../data', train=True, download=True, transform=transform)
-    test_dataset = datasets.MNIST('../data', train=False, transform=transform)
+    train_dataset = datasets.MNIST(
+        "../data", train=True, download=True, transform=transform
+    )
+    test_dataset = datasets.MNIST("../data", train=False, transform=transform)
 
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=args.test_batch_size, shuffle=False)
+    test_loader = DataLoader(
+        test_dataset, batch_size=args.test_batch_size, shuffle=False
+    )
 
     model = Net().to(device)
 
     if args.resume:
-        if os.path.isfile('model_checkpoint.pt'):
-            print('Loading model checkpoint...')
-            checkpoint = torch.load('model_checkpoint.pt')
-            model.load_state_dict(checkpoint['model_state_dict'])
+        if os.path.isfile("model_checkpoint.pt"):
+            print("Loading model checkpoint...")
+            checkpoint = torch.load("model_checkpoint.pt")
+            model.load_state_dict(checkpoint["model_state_dict"])
         else:
-            print('No model checkpoint found. Starting from scratch.')
+            print("No model checkpoint found. Starting from scratch.")
 
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
         train_epoch(epoch, args, model, device, train_loader, optimizer)
         test_epoch(model, device, test_loader)
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-        }, 'model_checkpoint.pth')
+        torch.save(
+            {
+                "epoch": epoch,
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+            },
+            "model_checkpoint.pth",
+        )
+
 
 if __name__ == "__main__":
     main()
