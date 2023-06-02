9d3299b95861a620c00421f1cc993b664ebf34c6
aiplaybookin <aiplaybook.in@gmail.com>
2023-06-02 17:15:07 +0000

    updated with first solution

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
-FROM ubuntu:latest
+FROM python:3.9-slim-bullseye
 
 WORKDIR /workspace
+
+COPY requirements.txt requirements.txt
+
+RUN pip3 install -r requirements.txt \
+    && rm -rf /root/.cache/pip
+
 COPY train.py /workspace/
 
-CMD ["python", "train.py"]
+CMD ["python3", "train.py"]
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import argparse
@@ -9,39 +10,106 @@ from torch.utils.data import DataLoader
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # TODO: Define your model architecture here
+        self.fc1 = nn.Linear(784, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
 
     def forward(self, x):
-        # TODO: Define the forward pass
-        pass
+        x = x.view(-1, 784)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1)
 
 def train_epoch(epoch, args, model, device, data_loader, optimizer):
-    # TODO: Implement the training loop here
-    pass
+    model.train()
+    for batch_idx, (data, target) in enumerate(data_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(data_loader.dataset)} '
+                  f'({100. * batch_idx / len(data_loader):.2f}%)]\tLoss: {loss.item():.6f}')
 
 def test_epoch(model, device, data_loader):
-    # TODO: Implement the testing loop here
-    pass
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in data_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(data_loader.dataset)
+    accuracy = 100. * correct / len(data_loader.dataset)
+    print(f'Test set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(data_loader.dataset)} '
+          f'({accuracy:.2f}%)')
 
 def main():
     # Parser to get command line arguments
     parser = argparse.ArgumentParser(description='MNIST Training Script')
-    # TODO: Define your command line arguments here
-    
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--resume', action='store_true', default=False,
+                        help='resume training from the last checkpoint')
+
     args = parser.parse_args()
-    use_cuda = torch.cuda.is_available()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
     torch.manual_seed(args.seed)
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    # TODO: Load the MNIST dataset for training and testing
-    
-    model = Net().to(device)
-    # TODO: Add a way to load the model checkpoint if 'resume' argument is True
+    # Load the MNIST dataset for training and testing
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
 
-    # TODO: Choose and define the optimizer here
-    
-    # TODO: Implement the training and testing cycles
-    # Hint: Save the model after each epoch
+    train_dataset = datasets.MNIST('../data', train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST('../data', train=False, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=args.test_batch_size, shuffle=False)
+
+    model = Net().to(device)
+
+    if args.resume:
+        if os.path.isfile('model_checkpoint.pt'):
+            print('Loading model checkpoint...')
+            checkpoint = torch.load('model_checkpoint.pt')
+            model.load_state_dict(checkpoint['model_state_dict'])
+        else:
+            print('No model checkpoint found. Starting from scratch.')
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    for epoch in range(1, args.epochs + 1):
+        train_epoch(epoch, args, model, device, train_loader, optimizer)
+        test_epoch(model, device, test_loader)
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+        }, 'model_checkpoint.pth')
 
 if __name__ == "__main__":
     main()
